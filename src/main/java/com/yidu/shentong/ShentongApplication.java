f465c1a0c7b599edf8d7140a0afc8c52cfc430fb
lixiaolong20190101 <2718893665@qq.com>
2020-12-25 09:34:43 +0800

    申通快递

---
+++ src/main/java/com/yidu/shentong/ShentongApplication.java
@@ -0,0 +1,13 @@
+package com.yidu.shentong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShentongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShentongApplication.class, args);
+    }
+
+}
